2dcb977be3e3ce7f81018bf0a87213a418a061e9
Nenad Mitic <nenad@folkmatic.com>
2016-09-08 09:10:18 +0200

    removed unused script

--- symfony3-php7/deploy/deploy-vagrant-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp deploy/app_dev.php web/app.php
-cp deploy/parameters-dev.yml app/config/parameters.yml
-rm -rf var/cache/*
-rm -fr web/bundles
-php bin/console doctrine:schema:update --force
-php bin/console assets:install web --symlink
